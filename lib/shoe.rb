028f1a76c5edbe1660933a4e2adf2bd8b9c75342
Jared S <jaredjames2020@gmail.com>
2019-09-23 23:36:56 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_writer :brand
+  attr_reader :brand
 
   BRANDS = []
 
@@ -13,7 +13,7 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
   
-  def brand=(brand)
+  def brand
     @brand = brand
     BRANDS << brand
   end
